5279a66f36d0799fac926a119bfe00cb55f56c13
Youssef George <ysfgrg2007@gmail.com>
2024-09-18 23:30:42 +0300

    fixing phone copy

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,7 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Footer
     fetch('../components/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer').innerHTML = data;
+
+    // Add the copy to clipboard functionality
+    const phoneLink = document.getElementById('phone-link');
+    phoneLink.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default action
+
+        const phoneNumber = phoneLink.getAttribute('data-phone');
+        navigator.clipboard.writeText(phoneNumber).then(function() {
+            alert("Phone number copied to clipboard: " + phoneNumber);
+        }, function(err) {
+            console.error("Could not copy text: ", err);
+        });
     });
+});
 });
